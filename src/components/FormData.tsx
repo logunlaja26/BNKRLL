db1a6c4ab8d0c3016af38b6c8d59eec49f082407
logunlaja26 <logunlaja26@gmail.com>
2023-12-05 18:18:15 -0600

    Updated FormData object to accomodate for dropdown selection

--- src/components/FormData.tsx
+++ src/components/FormData.tsx
@@ -1,12 +1,12 @@
 export interface FormData {
   name: string;
   email: string;
-  limit: string[];
-  payType: string[];
+  limit: string;
+  payType: string;
   gameType: string;
   location: string;
   buyin: number;
-  profit: number;
+  gameProfit: number;
   message: string;
 }
 
